482e6d15f0111d45c985fe60993c507a08bddd37
Kelly2512008 <71078075+Kelly2512008@users.noreply.github.com>
2020-12-24 15:13:14 +0530

    Add files via upload

--- p5.play-boilerplate-master 6/sketch.js
+++ p5.play-boilerplate-master 6/sketch.js
@@ -1,13 +1,16 @@
 var rapunzel,rapunzelimg;
 var invisibleGround;
-var warriorsGroup,lanternsGroup;
+var warriorsGroup,lanternsGroup,warriorimg,crownimg;
 var life = 3;
 var gameState="start";
-var  startButton;
-var restartButton;
+var  startButton,startButtonImg;
+var restartButton,restartButtonImg;
 function preload(){
   rapunzelimg=loadImage("images/rapunzel.png");
-
+  warriorimg=loadImage("images/warrior.png");
+  crownimg=loadImage("images/Crown.jpg");
+  startButtonImg=loadImage("images/playButton.jpg");
+  restartButtonImg=loadImage("images/resetButton.png");
 }
 function setup() {
   createCanvas(displayWidth,displayHeight);
@@ -21,12 +24,18 @@ function setup() {
   lanternsGroup=new Group();
   
   startButton=createSprite(displayWidth/2,displayHeight/2);
+  startButton.addImage(startButtonImg);
   restartButton=createSprite(displayWidth/2,displayHeight/2+30)
+  restartButton.addImage(restartButtonImg);
   restartButton.visible=false;
+  
 }
 
 function draw() {
   background(0,0,0);
+  textSize(14)
+  fill("white")
+  text("LIVES:"+life,displayWidth-100,50)
   console.log(displayHeight)
 
   if(gameState==="start" && mousePressedOver(startButton)){
@@ -35,22 +44,30 @@ function draw() {
   
   }  
   if(gameState==="play"){
-  if(keyDown("space")&&rapunzel.y>=displayHeight-135){
+  if(touches.length>0||keyDown("space")&&rapunzel.y>=displayHeight-135){
     rapunzel.velocityY=-10;
-
+    touches=[];
   }
+
   rapunzel.velocityY=rapunzel.velocityY+0.5
   
   spawnWarriors();
   spawnLanterns();
+  if(rapunzel.isTouching(lanternsGroup)){
+    life=life+1;
+  }
   if(rapunzel.isTouching(warriorsGroup)){
     if(life>0){
       life=life-1;
       gameState="start"
+      startButton.visible=true;
     }
     else if(life===0){
       gameState="end";
+      restartButton.visible=true;
     }
+    warriorsGroup.destroyEach();
+    lanternsGroup.destroyEach();
   }
 }
 if(gameState==="end"){
@@ -68,6 +85,7 @@ rapunzel.collide(invisibleGround);
 function spawnWarriors(){
   if(frameCount%100===0){
     var warrior=createSprite(displayWidth,displayHeight-50);
+    warrior.addImage(warriorimg);
     warrior.velocityX=-6
   warriorsGroup.add(warrior)
   }
@@ -75,6 +93,7 @@ function spawnWarriors(){
 function spawnLanterns(){
   if(frameCount%1000===0){
     var lantern=createSprite(displayWidth,displayHeight-200);
+    lantern.addImage(crownimg);
     lantern.velocityX=-6
   lanternsGroup.add(lantern)
   }
